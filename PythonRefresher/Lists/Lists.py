8f042f27f04c1750658d1f614211b30e8dab7573
Adrian Zabolitzki <adrianzab@gmail.com>
2024-03-12 11:23:05 -0600

    Add List practice

--- PythonRefresher/Lists/Lists.py
+++ PythonRefresher/Lists/Lists.py
@@ -5,15 +5,32 @@ Lists are a collection of data
 
 my_list = [80, 96, 72, 100, 8]
 print(my_list)
-my_list.append(1000)
+print(my_list[1]) # just like in JavaScript, the index prints the element in that index position
+
+print(my_list[-1]) # the -1 index is the first element from the end
+
+my_list[0] = 25 # just like in JS, this reassigns index 0
 print(my_list)
-my_list.insert(2, 1000)
+
+print(len(my_list)) # this is Python way of finding length of array
+
+print(my_list[0:2]) # Python way of slicing (prints 0, 1, up to but not including 2)
+
+# INSERT AND DELETE
+
+my_list.append(1000) # inserts 1000 at the end of array
 print(my_list)
-my_list.remove(8)
+
+my_list.insert(2, 1000) # inserts at a specific index
 print(my_list)
-my_list.pop(0)
+
+my_list.remove(8) # deletes the value from the array
 print(my_list)
-my_list.sort()
+
+my_list.pop(0) # removes the value at index 0
+print(my_list)
+
+my_list.sort() # sorts numerically smallest to highest
 print(my_list)
 
 

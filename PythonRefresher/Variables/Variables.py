e259055e6e55802d2330e529aa9f335dc9efc307
Adrian Zabolitzki <adrianzab@gmail.com>
2024-03-12 10:03:07 -0600

    Add variables notes

--- PythonRefresher/Variables/Variables.py
+++ PythonRefresher/Variables/Variables.py
@@ -2,10 +2,20 @@
 Variables
 """
 
-first_name = "Eric"
+cost = 10 # integer
+tax_percent = .25 # floating point number
+tax = cost * tax_percent
+price = cost + tax
+print(price)
+
+username = "Codingwithroby"
+first_name = "Eric" # strings can use single or double quotes
 print(first_name)
 first_name = "Melissa"
 print(first_name)
+
+print(username + " " + first_name) # firs-name is reassigned to Melissa
+
 
 
 
